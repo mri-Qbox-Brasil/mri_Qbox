3033db0bde164bee238633a1e6c0e8cc38b8bf8c
kaJseia <89431075+mur4i@users.noreply.github.com>
2024-05-12 02:49:35 -0300

    Update para nova atualização

--- modules/mri/client-side/ox_lib-modules/context.lua
+++ modules/mri/client-side/ox_lib-modules/context.lua
@@ -1,3 +1,5 @@
+local color = '#51CF66'
+local imageUrl = 'https://media.discordapp.net/attachments/1227489601925550150/1236525641973764096/Brasil_96_x_96_px_2.png?ex=663f93fe&is=663e427e&hm=afd1c2a4814d090fd4312d39aaf90bc71991a38be3450f5b0c532743e19bec7f&=&format=webp&quality=lossless&width=24&height=24'
 
 -- Função para abrir o menu do jogador
 function AbrirMenuJogador()
@@ -18,14 +20,14 @@ function AbrirMenuJogador()
         grade = PlayerData.gang.grade.name
     }
     
-    local imageUrl = 'https://media.discordapp.net/attachments/1227489601925550150/1236525641973764096/Brasil_96_x_96_px_2.png?ex=663f93fe&is=663e427e&hm=afd1c2a4814d090fd4312d39aaf90bc71991a38be3450f5b0c532743e19bec7f&=&format=webp&quality=lossless&width=24&height=24'
     lib.registerContext({
         id = 'menu_jogador',
-        title = '![logo]('..imageUrl..') mri Qbox Brasil',
+        title = '![logo]('..imageUrl..') **mri Qbox Brasil**',
         options = {
             {
                 title = 'Identificação',
                 icon = 'fas fa-address-card',
+                iconColor = color,
                 description =   '**ID**: '..PlayerData.source..' | '..
                                 '**RG**: '..PlayerData.citizenid..'                                                                                                '..
                                 '**Nome**: '..playerData.name,
@@ -36,6 +38,7 @@ function AbrirMenuJogador()
             {
                 title = 'Emprego',
                 icon = 'fas fa-briefcase',
+                iconColor = color,
                 description = jobData.label..' | '..jobData.grade,
                 onSelect = function()
                     ExecuteCommand('job')
@@ -44,6 +47,7 @@ function AbrirMenuJogador()
             {
                 title = 'Gangue',
                 icon = 'gun',
+                iconColor = color,
                 description = gangData.label..' | '..gangData.grade,
                 onSelect = function()
                     ExecuteCommand('gang')
@@ -53,6 +57,8 @@ function AbrirMenuJogador()
                 title = 'Ver Habilidades',
                 description = 'Exibir seu progresso em habilidades do servidor.',
                 icon = 'book-tanakh',
+                iconColor = color,
+                arrow = true,
                 -- menu = 'skill_menu',
                 onSelect = function()
                     ExecuteCommand('rep')
@@ -69,13 +75,13 @@ Citizen.CreateThread(function()
     -- Menu admin (F10)
     lib.registerContext({
         id = 'menu_admin',
-        title = 'Administração',
+        title = '![logo]('..imageUrl..') Administração',
         options = {
             {
                 title = 'Abrir Painel',
                 description = 'Painel da Administração do Servidor',
                 icon = 'fa-solid fa-user-tie',
-
+                iconColor = color,
                 onSelect = function()
                     ExecuteCommand('adm')
                 end
@@ -84,6 +90,7 @@ Citizen.CreateThread(function()
                 title = 'Customizar Veículo',
                 description = 'Tune seu veículo atual',
                 icon = 'palette',
+                iconColor = color,
                 onSelect = function()
                     ExecuteCommand('customs')
                 end
@@ -92,6 +99,8 @@ Citizen.CreateThread(function()
                 title = 'Gerenciamento',
                 description = 'Acesso a opções de gerenciamento do servidor',
                 icon = 'fa-solid fa-cogs',
+                iconColor = color,
+                iconAnimation = 'fade',
                 menu = 'menu_gerencial'
             }
         }
@@ -104,33 +113,45 @@ Citizen.CreateThread(function()
         title = 'Gerenciamento',
         options = {
             {
-                title = 'Criar Portas',
-                description = 'Gerencie as Portas criadas',
+                title = 'Portas',
+                description = 'Crie ou gerencie as trancas de portas e portões do servidor.',
                 icon = 'door-closed',
+                iconColor = color,
+                iconAnimation = 'fade',
+                arrow = true,
                 onSelect = function()
                     ExecuteCommand('doorlock')
                 end
             },
             {
-                title = 'Criar Blips',
-                description = 'Gerencie os blips criados',
+                title = 'Blips',
+                description = 'Crie ou gerencie todos os blips, você pode copiar as configurações de um já criado.  ',
                 icon = 'location-dot',
+                iconColor = color,
+                iconAnimation = 'fade',
+                arrow = true,
                 onSelect = function()
                     ExecuteCommand('blip')
                 end
             },
             {
-                title = 'Criar Baú',
-                description = 'Gerencie os baús criados',
+                title = 'Baús',
+                description = 'Crie ou gerencie os baús do servidor, você pode restringir por permissões ou senha.',
                 icon = 'box',
+                iconColor = color,
+                iconAnimation = 'fade',
+                arrow = true,
                 onSelect = function()
                     ExecuteCommand('bau')
                 end
             },
             {
-                title = 'Criar NPC',
-                description = 'Gerencie os NPCs criados',
+                title = 'NPC',
+                description = 'Crie ou gerencie os NPCs do servidor, você pode colocar animações nos NPCs.',
                 icon = 'users-gear',
+                iconColor = color,
+                iconAnimation = 'fade',
+                arrow = true,
                 onSelect = function()
                     ExecuteCommand('npc')
                 end
@@ -139,6 +160,8 @@ Citizen.CreateThread(function()
                 title = 'Criar Props',
                 description = 'Gerencie os props criados',
                 icon = 'tree',
+                iconColor = color,
+                arrow = true,
                 onSelect = function()
                     ExecuteCommand('objectspawner')
                 end
@@ -147,6 +170,8 @@ Citizen.CreateThread(function()
                 title = 'Criar Elevador',
                 description = 'Gerencie os elevadores criados',
                 icon = 'elevator',
+                iconColor = color,
+                arrow = true,
                 onSelect = function()
                     ExecuteCommand('elevador')
                 end
@@ -155,6 +180,8 @@ Citizen.CreateThread(function()
                 title = 'Criar Outdoors/Posters',
                 description = 'Gerencie as imagens criadas',
                 icon = 'panorama',
+                iconColor = color,
+                arrow = true,
                 onSelect = function()
                     ExecuteCommand('poster')
                 end
