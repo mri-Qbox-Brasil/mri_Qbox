84ead4b2c1666d00c8f2c89b5c81273d74cb709c
Gabriel Filipe Guimaraes de Freitas <ggfto@outlook.com>
2024-05-13 16:23:14 -0300

    [FIX] remocao imagem comentada

--- modules/mri/client-side/ox_lib-modules/context.lua
+++ modules/mri/client-side/ox_lib-modules/context.lua
@@ -1,5 +1,4 @@
 local color = '#51CF66'
--- local imageUrl = 'https://media.discordapp.net/attachments/1227489601925550150/1236525641973764096/Brasil_96_x_96_px_2.png?ex=663f93fe&is=663e427e&hm=afd1c2a4814d090fd4312d39aaf90bc71991a38be3450f5b0c532743e19bec7f&=&format=webp&quality=lossless&width=24&height=24'
 local imageUrl = 'https://cfx-nui-mri_Qbox/web-side/icones/logo24.png'
 
 -- Função para abrir o menu do jogador
