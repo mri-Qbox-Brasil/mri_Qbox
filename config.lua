ee4057738c28629d66d01c4cafe6ed697ef063c4
kaJseia <89431075+mur4i@users.noreply.github.com>
2024-08-31 02:37:24 -0300

    Update: add mercosul plates options

--- config.lua
+++ config.lua
@@ -22,10 +22,10 @@ cfg.dropitems_table = {
 
 cfg.SqlBackup = {
     Active = false,
-    Debug = false,
-    BackupCommand = 'backupdb',
-    BackupOnStart = false,
-    -- ExecuteOnTime = {
+    Debug = false,                          -- true para mostrar logs no console
+    BackupCommand = 'backupdb',             -- comando para backup do banco de dados
+    BackupOnStart = false,                  -- true para criar backup ao iniciar o servidor
+    -- ExecuteOnTime = {                    -- hora e minuto para executar o backup
     --     hour = 3,
     --     min = 40
     -- }
@@ -35,11 +35,11 @@ cfg.SqlBackup = {
 -----------------------------------------------------------------------------------------
 
 cfg.entervehicle = {
-    toggle = true,                       -- true para ativar entrar na porta que você estiver olhando pelo target
+    toggle = true,                          -- true para ativar entrar na porta que você estiver olhando pelo target
 }
 
 cfg.dumpsters = {
-    toggle = true,                      -- true para ativar as lixeiras para abrir no olhinho
+    toggle = true,                          -- true para ativar as lixeiras para abrir no olhinho
     TrashCans = {
         Model = {218085040, 666561306, -58485588, -206690185, 1511880420, 682791951}, -- modelo da lixeira
     }
@@ -49,37 +49,37 @@ cfg.dumpsters = {
 ---- COMBAT-MODULES
 -----------------------------------------------------------------------------------------
 cfg.disablefreepunch = {
-    toggle = false,                      -- true para desativar o soco se não estiver mirando com o botão direito (corrige bug da galera iniciante se bater sem querer usando o olhinho em tarefas aleatórias)
+    toggle = false,                         -- true para desativar o soco se não estiver mirando com o botão direito (corrige bug da galera iniciante se bater sem querer usando o olhinho em tarefas aleatórias)
 }
 
 cfg.forcedfirstperson = {
-    twopov = false,                      -- true para forçar apenas 2 tipos de VISÕES (PRIMEIRA PESSOA E TERCEIRA PESSOA) - ao invés daquelas 4 opções de câmera padrão
+    twopov = false,                         -- true para forçar apenas 2 tipos de VISÕES (PRIMEIRA PESSOA E TERCEIRA PESSOA) - ao invés daquelas 4 opções de câmera padrão
     invehicle = {
-        ativar = false,                  -- true se quiser ativar forçar primeira pessoa no carro
-        hold = false,                    -- true se quiser que só fique em primeira pessoa se estiver segurando a arma
+        ativar = false,                     -- true se quiser ativar forçar primeira pessoa no carro
+        hold = false,                       -- true se quiser que só fique em primeira pessoa se estiver segurando a arma
     }
 }
 
 cfg.disablecombatroll = {
-    toggle = false,                      -- true para desativar o rolamento e não ser uma cidade Pvpas
+    toggle = false,                         -- true para desativar o rolamento e não ser uma cidade Pvpas
 }
 
 cfg.damageragdoll = {
-    toggle = false,                      -- true para ativar cair/tropeçar ao tomar tiro na perna
+    toggle = false,                         -- true para ativar cair/tropeçar ao tomar tiro na perna
 }
 
 cfg.disableblindfiring = {
-    toggle = false,                      -- true para desativar o tiro cego quando estiver pegando cover em paredes
+    toggle = false,                         -- true para desativar o tiro cego quando estiver pegando cover em paredes
 }
 
 cfg.realisticrecoil = {
-    hideCrosshair = false, -- Hide builtin GTA crosshair while aiming?
-    drunkAiming = false, -- Enable "drunk" aiming?
-    verticalRecoil = false, -- Enable realistic vertical recoil while shooting?
-    disableAimPunching = false, -- Disables punching with [R] and other keys while aiming
-    disableHeadshots = false, -- Disables one-shots to head
-    drunkAimingPower = 0.20, -- Higher number = Higher screen shake
-    whitelistedWeapons = {  -- Table of weapons with no recoil
+    hideCrosshair = false,                  -- Hide builtin GTA crosshair while aiming?
+    drunkAiming = false,                    -- Enable "drunk" aiming?
+    verticalRecoil = false,                 -- Enable realistic vertical recoil while shooting?
+    disableAimPunching = false,             -- Disables punching with [R] and other keys while aiming
+    disableHeadshots = false,               -- Disables one-shots to head
+    drunkAimingPower = 0.20,                -- Higher number = Higher screen shake
+    whitelistedWeapons = {                  -- Table of weapons with no recoil
     ["WEAPON_SNIPERRIFLE"] = true,
     ["WEAPON_HEAVYSNIPER"] = true,
     ["WEAPON_HEAVYSNIPER_MK2"] = true,
@@ -87,15 +87,15 @@ cfg.realisticrecoil = {
     ["WEAPON_MARKSMANRIFLE_MK2"] = true,
     -- Add more weapons if needed
     },
-    recoilMultipliers = {   -- [ONLY FOR VERTICAL RECOIL] Edit the power of the recoil for each weapon type
-                            -- 0.0 = none (default)
+    recoilMultipliers = {                   -- [ONLY FOR VERTICAL RECOIL] Edit the power of the recoil for each weapon type
+                                            -- 0.0 = none (default)
         ["PISTOL"] = 0.3,
         ["SMG"] = 0.8,
         ["RIFLE"] = 1.3,
         ["LMG"] = 1.6,
         ["SHOTGUN"] = 2.3,
         ["SNIPER"] = 5.5,
-        ["VEHICLE"] = 0.8   -- Recoil while in vehicle
+        ["VEHICLE"] = 0.8                   -- Recoil while in vehicle
     },
 }
 
@@ -103,27 +103,31 @@ cfg.realisticrecoil = {
 ---- VEHICLES-MODULES
 -----------------------------------------------------------------------------------------
 cfg.wheelbreak = {
-    toggle = false,                     -- true para ativar as rodas quebrarem/soltarem ao bater o veículo
-    speed = 150,                        -- velocidade em km/h que a roda vai quebrar/soltar (100 = hardcore)
+    toggle = false,                         -- true para ativar as rodas quebrarem/soltarem ao bater o veículo
+    speed = 150,                            -- velocidade em km/h que a roda vai quebrar/soltar (100 = hardcore)
 }
 
 cfg.savewheelpos = {
-    toggle = false,                     -- true para ativar a roda ficar parada em um ângulo específico ao sair do veículo
+    toggle = false,                         -- true para ativar a roda ficar parada em um ângulo específico ao sair do veículo
 }
 
 cfg.disableaircontrol = {
-    toggle = false,                     -- true para ativar a roda ficar parada em um ângulo específico ao sair do veículo
+    toggle = false,                         -- true para ativar a roda ficar parada em um ângulo específico ao sair do veículo
 }
 
 cfg.carexplosion = {
-    toggle = false,                     -- true para ativar a Explosão de veículos ao cair de uma certa altura
-    height = 40,                        -- qual altura você quer para ativar a explosão
+    toggle = false,                         -- true para ativar a Explosão de veículos ao cair de uma certa altura
+    height = 40,                            -- qual altura você quer para ativar a explosão
 }
 
 cfg.drift = {
-    toggle = true,                      -- ativar drift usando shift
-    points = false,                      -- ativar drift points nui
-    speed = 80,                         -- velocidade máxima que pode usar o drift
+    toggle = true,                          -- ativar drift usando shift
+    points = false,                         -- ativar drift points nui
+    speed = 80,                             -- velocidade máxima que pode usar o drift
+}
+
+cfg.mercosulplates = {
+    toggle = true,                          -- true para ativar placas do mercosul nos veículos
 }
 
 return cfg
---
+++ modules/mri/client-side/vehicles-modules/mercosulplates.lua
@@ -0,0 +1,22 @@
+if not Config.mercosulPlates then return end
+
+imageUrl = "https://assets.mriqbox.com.br/dui/platebr_image.png" 
+
+local textureDic = CreateRuntimeTxd('duiTxd')
+local object = CreateDui(imageUrl, 540, 300)
+local handle = GetDuiHandle(object)
+CreateRuntimeTextureFromDuiHandle(textureDic, "duiTex", handle)
+AddReplaceTexture('vehshare', 'plate01', 'duiTxd', 'duiTex')
+AddReplaceTexture('vehshare', 'plate02', 'duiTxd', 'duiTex')
+AddReplaceTexture('vehshare', 'plate03', 'duiTxd', 'duiTex') 
+AddReplaceTexture('vehshare', 'plate04', 'duiTxd', 'duiTex')
+AddReplaceTexture('vehshare', 'plate05', 'duiTxd', 'duiTex') 
+
+local object = CreateDui('https://assets.mriqbox.com.br/dui/platebr_object.png', 540, 300) 
+local handle = GetDuiHandle(object)
+CreateRuntimeTextureFromDuiHandle(textureDic, "duiTex2", handle) 
+AddReplaceTexture('vehshare', 'plate01_n', 'duiTxd', 'duiTex2')
+AddReplaceTexture('vehshare', 'plate02_n', 'duiTxd', 'duiTex2')
+AddReplaceTexture('vehshare', 'plate03_n', 'duiTxd', 'duiTex2') 
+AddReplaceTexture('vehshare', 'plate04_n', 'duiTxd', 'duiTex2')
+AddReplaceTexture('vehshare', 'plate05_n', 'duiTxd', 'duiTex2')
