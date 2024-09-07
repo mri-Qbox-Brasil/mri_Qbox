4c6353d4dfd2d286d6a95a1354994a2b0d71ac40
Gabriel Filipe Guimaraes de Freitas <ggfto@outlook.com>
2024-09-06 23:48:33 -0300

    Feat: Geração automática de release
    - Capacidade de extrair arquivos zip(auto-update).

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,30 +1,30 @@
 fx_version "cerulean"
-
-author "Murai Dev"
-discord ".mur4i"
-description 'Atualizações para mri-Qbox'
-
 game "gta5"
+
+description "MRIQBOX_DESCRIPTION"
+author "MRI QBOX Team"
+discord "MRIQBOX_DISCORD"
+version "MRIQBOX_VERSION"
+
 ui_page "web-side/index.html"
 
 shared_scripts {
-	'@ox_lib/init.lua',
-	'@qbx_core/modules/playerdata.lua',
+	"@ox_lib/init.lua",
+	"@qbx_core/modules/playerdata.lua",
 	"config.lua",
-
 	"**/**/config.lua",
 	"**/**/shared/*",
+}
+
+server_scripts {
+    "@oxmysql/lib/MySQL.lua",
+    "**/**/server/*",
+    "**/**/server-side/**/*",
 }
 
 client_scripts {
 	"**/**/client/*",
 	"**/**/client-side/**/*",
-}
-
-server_scripts {
-    '@oxmysql/lib/MySQL.lua',
-	"**/**/server/*",
-	"**/**/server-side/**/*",
 }
 
 files {
