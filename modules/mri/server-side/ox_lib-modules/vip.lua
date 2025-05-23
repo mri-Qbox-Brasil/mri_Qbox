12868d38c6e82f0ecc2026448b03ff06dd8351ec
Gabriel Filipe Guimaraes de Freitas <ggfto@outlook.com>
2025-05-23 18:56:49 -0300

    fix source missing

--- modules/mri/server-side/ox_lib-modules/vip.lua
+++ modules/mri/server-side/ox_lib-modules/vip.lua
@@ -66,7 +66,7 @@ local function updateInventoryWeight(source)
     end
 end
 
-local function executeVip(args)
+local function executeVip(source, args)
     if not args.id then
         sendNotification(source, "error", "Id não informado.")
         return
