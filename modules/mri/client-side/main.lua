061fe48e99249ef8913f1d69ee21715ac7a33edc
Gabriel Freitas <ggfto@outlook.com>
2025-11-20 14:58:14 -0300

    Remoção de código legado
    Pequenos ajustes

---
+++ modules/mri/client-side/main.lua
@@ -0,0 +1,8 @@
+local ColorScheme = {
+    success = '#51CF66',
+    info = '#668CFF',
+    warning = '#FFD700',
+    danger = '#FF6347'
+}
+
+GlobalState:set('UIColors', ColorScheme, true)
--- modules/mri/server-side/utilities-modules/commands.lua
+++ modules/mri/server-side/utilities-modules/commands.lua
@@ -69,3 +69,17 @@ lib.addCommand('raycast', {
         description = "Coordendas copiadas para a Clipboard (Área de Transferência)."
     })
 end)
+
+lib.addCommand('models', {
+    help = 'List all missing models',
+    restricted = 'group.admin'
+}, function(source)
+    local models = GetAllVehicleModels()
+    local missingVehicles = {}
+    for i = 1, #models do
+        if not VehicleList[models[i]] then
+            missingVehicles[#missingVehicles+1] = models[i]
+        end
+    end
+    lib.setClipboard(json.encode(missingVehicles))
+end)
